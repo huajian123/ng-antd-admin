a76a1787590072c61771e89031112c54afb475a7
huajian <287643967@qq.com>
2020-12-22 13:35:11 +0800

    代码提交

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import {CoreModule} from './core/core.module';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {ShareModule} from './share/share.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import {ShareModule} from './share/share.module';
   imports: [
     CoreModule,
     ShareModule,
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
   ],
--- src/app/share/shared-zorro.module.ts
+++ src/app/share/shared-zorro.module.ts
@@ -1,6 +1,18 @@
 import {IconsProviderModule} from './icons-provider.module';
+import {NzFormModule} from 'ng-zorro-antd/form';
+import {NzButtonModule} from 'ng-zorro-antd/button';
+import {NzInputModule} from 'ng-zorro-antd/input';
+import {NzSelectModule} from 'ng-zorro-antd/select';
+import {NzTabsModule} from 'ng-zorro-antd/tabs';
+import {NzCheckboxModule} from 'ng-zorro-antd/checkbox';
 
 
 export const SHARED_ZORRO_MODULES = [
-  IconsProviderModule
+  IconsProviderModule,
+  NzFormModule,
+  NzButtonModule,
+  NzInputModule,
+  NzSelectModule,
+  NzTabsModule,
+  NzCheckboxModule,
 ];
