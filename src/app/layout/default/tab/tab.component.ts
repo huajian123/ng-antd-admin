45e879d8fd95ba03577c91633cfac4899aaa74a3
huajian123 <287643967@qq.com>
2020-12-22 22:09:35 +0800

    修改tabs

--- src/app/layout/default/tab/tab.component.ts
+++ src/app/layout/default/tab/tab.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {Component, OnInit, ChangeDetectionStrategy} from '@angular/core';
+import {TabModel, TabService} from '../../../core/services/tab.service';
+import {NzContextMenuService, NzDropdownMenuComponent} from 'ng-zorro-antd/dropdown';
+import {ThemeService} from '../../../core/services/theme.service';
 
 @Component({
   selector: 'app-tab',
@@ -8,7 +11,54 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 })
 export class TabComponent implements OnInit {
 
-  constructor() { }
+  tabs = this.tabService.getTabArray();
+  themesOptions$ = this.themesService.getThemesMode();
+
+  constructor(public tabService: TabService, private nzContextMenuService: NzContextMenuService,
+              private themesService: ThemeService) {
+  }
+
+  get currentIndex(): number {
+    return this.tabService.getCurrentTabIndex();
+  }
+
+  public trackByTab(index: number, tab: TabModel): string {
+    return tab.title;
+  }
+
+  stopMounseEvent(e: MouseEvent): void {
+    e.stopPropagation();
+    e.preventDefault();
+  }
+
+  // 右键点击关闭右侧tab
+  closeRithTab(tab: TabModel, e: MouseEvent, index: number): void {
+    this.stopMounseEvent(e);
+    this.tabService.delRightTab(tab.path, index);
+  }
+
+  // 关闭其他tab
+  closeOtherTab(tab: TabModel, e: MouseEvent, index: number): void {
+    this.stopMounseEvent(e);
+    this.tabService.delOtharTab(tab.path, index);
+  }
+
+  // 关闭当前Tab
+  closeTab(tab: TabModel, e: MouseEvent, index: number): void {
+    this.stopMounseEvent(e);
+    if (1 === this.tabs.length) {
+      return;
+    }
+    this.tabService.delTab(tab.path, index);
+  }
+
+  contextMenu($event: MouseEvent, menu: NzDropdownMenuComponent): void {
+    this.nzContextMenuService.create($event, menu);
+  }
+
+  closeMenu(): void {
+    this.nzContextMenuService.close();
+  }
 
   ngOnInit(): void {
   }
