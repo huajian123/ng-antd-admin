cd3cbdffeaf774234ae151defa218252d53e5edd
huajian <287643967@qq.com>
2020-12-25 17:10:47 +0800

    修改dark主题样式

---
+++ src/app/utils/tools.ts
@@ -0,0 +1,27 @@
+const fnGetFile = function getFile(url: string, isBlob = false): Promise<any> {
+  return new Promise((resolve, reject) => {
+    const client = new XMLHttpRequest();
+    client.responseType = isBlob ? 'blob' : '';
+    client.onreadystatechange = () => {
+      if (client.readyState !== 4) {
+        return;
+      }
+      if (client.status === 200) {
+        const urlArr = client.responseURL.split('/');
+        resolve({
+          data: client.response,
+          url: urlArr[urlArr.length - 1]
+        });
+      } else {
+        reject(new Error(client.statusText));
+      }
+    };
+    client.open('GET', url);
+    client.send();
+  });
+};
+
+
+export {
+  fnGetFile
+};
