b8f1cb5d71a8f44adc64c76bb6eccbdf7a93d5f9
huajian <287643967@qq.com>
2021-01-21 23:10:20 +0800

    代码提交

--- src/app/core/services/common/reuse-strategy.ts
+++ src/app/core/services/common/reuse-strategy.ts
@@ -1,7 +1,7 @@
 import {RouteReuseStrategy, ActivatedRouteSnapshot, DetachedRouteHandle} from '@angular/router';
 
 // 参考https://zhuanlan.zhihu.com/p/29823560
-
+// https://blog.csdn.net/weixin_30561425/article/details/96985967?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.control&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.control
 export class SimpleReuseStrategy implements RouteReuseStrategy {
 
   static handlers: { [key: string]: any } = {};
