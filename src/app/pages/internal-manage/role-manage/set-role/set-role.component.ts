8936821457a45c204449b5ee2320db995f83ef46
huajian <287643967@qq.com>
2021-01-24 21:23:15 +0800

    修改settings样式

--- src/app/pages/internal-manage/role-manage/set-role/set-role.component.ts
+++ src/app/pages/internal-manage/role-manage/set-role/set-role.component.ts
@@ -1,14 +1,22 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {PageHeaderType} from "../../../../share/components/page-header/page-header.component";
 
 @Component({
   selector: 'app-set-role',
   templateUrl: './set-role.component.html',
   styleUrls: ['./set-role.component.less'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation:ViewEncapsulation.None
 })
 export class SetRoleComponent implements OnInit {
+  pageHeaderInfo: Partial<PageHeaderType> = {
+    title: '设置权限',
+    desc:'当前角色：超级管理员',
+    breadcrumb: ['首页', '内部管理', '角色管理', '设置权限']
+  };
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
