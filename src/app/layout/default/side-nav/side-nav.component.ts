b2d62a651208df114db08f5e657ec795ae70a57a
huajian <287643967@qq.com>
2021-02-03 10:18:28 +0800

    重构主题系统3

--- src/app/layout/default/side-nav/side-nav.component.ts
+++ src/app/layout/default/side-nav/side-nav.component.ts
@@ -14,6 +14,7 @@ import {TabService} from '../../../core/services/common/tab.service';
 export class SideNavComponent implements OnInit {
 
   themesOptions$ = this.themesService.getThemesMode();
+  isNightTheme$ = this.themesService.getIsNightTheme();
   @Input() isCollapsed = false;
 
 
