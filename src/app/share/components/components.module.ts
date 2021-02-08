df080a883ad57e3270f534edf4289fbb4a83c791
huajian <287643967@qq.com>
2021-02-08 14:42:10 +0800

    添加顶部进度条

--- src/app/share/components/components.module.ts
+++ src/app/share/components/components.module.ts
@@ -2,8 +2,9 @@ import {NgModule} from '@angular/core';
 import {PageHeaderModule} from './page-header/page-header.module';
 import {AntTableModule} from './ant-table/ant-table.module';
 import {FooterSubmitModule} from './footer-submit/footer-submit.module';
+import {TopProgressBarModule} from './top-progress-bar/top-progress-bar.module';
 
-const MODULES = [FooterSubmitModule, PageHeaderModule, AntTableModule];
+const MODULES = [FooterSubmitModule, PageHeaderModule, AntTableModule, TopProgressBarModule];
 
 @NgModule({
   declarations: [],
--- src/app/share/components/top-progress-bar/top-progress-bar.component.ts
+++ src/app/share/components/top-progress-bar/top-progress-bar.component.ts
@@ -1,8 +1,8 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component} from '@angular/core';
 import {
   NavigationCancel,
   NavigationEnd,
-  NavigationError,
+  NavigationError, NavigationStart,
   RouteConfigLoadEnd,
   RouteConfigLoadStart,
   Router
@@ -12,26 +12,30 @@ import {
   selector: 'app-top-progress-bar',
   templateUrl: './top-progress-bar.component.html',
   styleUrls: ['./top-progress-bar.component.less'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TopProgressBarComponent {
   isFetching = false;
 
-  constructor(private  router: Router) {
+  constructor(private  router: Router, private cdr: ChangeDetectorRef) {
     // scroll to top in change page
     router.events.subscribe(evt => {
       // 表示在惰性加载某个路由配置前触发的事件。
+      console.log(!this.isFetching && evt instanceof RouteConfigLoadStart);
       if (!this.isFetching && evt instanceof RouteConfigLoadStart) {
         this.isFetching = true;
+        this.cdr.markForCheck();
       }
-      /*   if (!this.isFetching && evt instanceof NavigationStart) {
-           this.isFetching = true;
-         }*/
+      if (!this.isFetching && evt instanceof NavigationStart) {
+        this.isFetching = true;
+        this.cdr.markForCheck();
+      }
       if (evt instanceof NavigationError || evt instanceof NavigationCancel) {
         this.isFetching = false;
         if (evt instanceof NavigationError) {
           // _message.error(`无法加载${evt.url}路由`, { nzDuration: 1000 * 3 });
         }
+        this.cdr.markForCheck();
         return;
       }
       if (!(evt instanceof NavigationEnd || evt instanceof RouteConfigLoadEnd)) {
@@ -40,7 +44,8 @@ export class TopProgressBarComponent {
       if (this.isFetching) {
         setTimeout(() => {
           this.isFetching = false;
-        }, 100);
+          this.cdr.markForCheck();
+        }, 300);
       }
     });
   }
