4dde2d972d7f0a34ed4d8e819331590662a068a7
huajian <287643967@qq.com>
2021-02-10 22:20:20 +0800

    代码提交

--- src/app/pages/dashboard/monitor/monitor.component.ts
+++ src/app/pages/dashboard/monitor/monitor.component.ts
@@ -2,6 +2,7 @@ import {AfterViewInit, ChangeDetectionStrategy, Component, OnInit} from '@angula
 import {FormBuilder} from '@angular/forms';
 import {Scene} from '@antv/l7';
 import {GaodeMap} from '@antv/l7-maps';
+import {Gauge} from '@antv/g2plot';
 
 
 @Component({
@@ -17,7 +18,47 @@ export class MonitorComponent implements OnInit, AfterViewInit {
   constructor(private fb: FormBuilder) {
   }
 
+  initDashBoard(): void {
+    const gauge = new Gauge('dashBoard', {
+      percent: 0.75,
+      autoFit: true,
+      height: 180,
+      range: {
+        color: '#30BF78',
+      },
+      indicator: {
+        pointer: {
+          style: {
+            stroke: '#D0D0D0',
+          },
+        },
+        pin: {
+          style: {
+            stroke: '#D0D0D0',
+          },
+        },
+      },
+      axis: {
+        label: {
+          formatter(v) {
+            return Number(v) * 100;
+          },
+        },
+        subTickLine: {
+          count: 3,
+        },
+      },
+      statistic: {
+        content: {
+          formatter: () => `87 %`,
+        },
+      },
+    });
+    gauge.render();
+  }
+
   ngAfterViewInit(): void {
+    // 地图
     const scene = new Scene({
       id: "map",
       logoPosition: 'bottomright',
@@ -28,6 +69,10 @@ export class MonitorComponent implements OnInit, AfterViewInit {
         center: [112, 23.69],
         zoom: 2.5
       })
+    });
+
+    setTimeout(() => {
+      this.initDashBoard();
     });
   }
 
