ef344774dd2e97cbb41e508ea84bd069dc481e33
huajian <287643967@qq.com>
2021-02-11 09:21:38 +0800

    新增监控页右侧板块

--- src/app/pages/dashboard/monitor/monitor.component.ts
+++ src/app/pages/dashboard/monitor/monitor.component.ts
@@ -2,7 +2,7 @@ import {AfterViewInit, ChangeDetectionStrategy, Component, OnInit} from '@angula
 import {FormBuilder} from '@angular/forms';
 import {Scene} from '@antv/l7';
 import {GaodeMap} from '@antv/l7-maps';
-import {Gauge} from '@antv/g2plot';
+import {Gauge, TinyArea} from '@antv/g2plot';
 
 
 @Component({
@@ -14,6 +14,30 @@ import {Gauge} from '@antv/g2plot';
 export class MonitorComponent implements OnInit, AfterViewInit {
   deadline = Date.now() + 1000 * 60 * 60 * 24 * 2 + 1000 * 30;
   scene!: Scene;
+  miniAreaData =[
+    264,
+    274,
+    284,
+    294,
+    300,
+    310,
+    320,
+    330,
+    340,
+    350,
+    360,
+    370,
+    380,
+    390,
+    400,
+    410,
+    420,
+    430,
+    440,
+    450,
+    460,
+    470,
+  ];
 
   constructor(private fb: FormBuilder) {
   }
@@ -57,11 +81,64 @@ export class MonitorComponent implements OnInit, AfterViewInit {
     gauge.render();
   }
 
+  initArea(): void {
+    const tinyArea = new TinyArea('miniArea', {
+      height: 120,
+      autoFit: true,
+      data:this.miniAreaData,
+      smooth: true,
+      areaStyle: {
+        fill: '#d6e3fd',
+      },
+      annotations: [
+        // 平均值
+        {
+          type: 'line',
+          start: ['min', 'mean'],
+          end: ['max', 'mean'],
+          text: {
+            content: '400亿元',
+            offsetY: -2,
+            style: {
+              textAlign: 'left',
+              fontSize: 10,
+             // fill: 'rgba(44, 53, 66, 0.45)',
+              textBaseline: 'bottom',
+            },
+          },
+          style: {
+         //   stroke: 'rgba(0, 0, 0, 0.25)',
+          },
+        },
+        // 目标值
+        {
+          type: 'line',
+          start: ['min', 800],
+          end: ['max', 800],
+          text: {
+            content: '1400亿元',
+            offsetY: -2,
+            style: {
+              textAlign: 'left',
+              fontSize: 10,
+             // fill: 'rgba(44, 53, 66, 0.45)',
+              textBaseline: 'bottom',
+            },
+          },
+          style: {
+           // stroke: 'rgba(0, 0, 0, 0.55)',
+          },
+        },
+      ],
+    });
+    tinyArea.render();
+
+  }
+
   ngAfterViewInit(): void {
-
-
     setTimeout(() => {
       this.initDashBoard();
+      this.initArea();
       // 地图
       const scene = new Scene({
         id: "map",
