9eb29e2905b80b12eff65b7b67fc5097863c3e53
huajian <287643967@qq.com>
2021-02-19 22:08:42 +0800

    添加个人设置页面主要框架

--- src/app/pages/personal/personal-setting/personal-setting.component.ts
+++ src/app/pages/personal/personal-setting/personal-setting.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {Component, OnInit, ChangeDetectionStrategy} from '@angular/core';
 
 @Component({
   selector: 'app-personal-setting',
@@ -7,8 +7,35 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PersonalSettingComponent implements OnInit {
+  menus: Array<{ key: string; title: string; selected?: boolean }> = [
+    {
+      key: 'base',
+      title: '基本设置',
+      selected: true,
+    },
+    {
+      key: 'security',
+      title: '安全设置',
+      selected: false,
+    },
+    {
+      selected: false,
+      key: 'binding',
+      title: '账号绑定',
+    },
+    {
+      selected: false,
+      key: 'notification',
+      title: '新消息通知',
+    },
+  ];
 
-  constructor() { }
+  constructor() {
+  }
+
+  to(item: { key: string }): void {
+    // this.router.navigateByUrl(`/pro/account/settings/${item.key}`);
+  }
 
   ngOnInit(): void {
   }
