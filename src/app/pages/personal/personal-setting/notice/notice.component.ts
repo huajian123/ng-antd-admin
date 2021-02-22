9d9a5f06643545c29ebeaca9a086fc515d8a4667
huajian <287643967@qq.com>
2021-02-22 16:23:39 +0800

    添加消息通知页面

--- src/app/pages/personal/personal-setting/notice/notice.component.ts
+++ src/app/pages/personal/personal-setting/notice/notice.component.ts
@@ -3,12 +3,22 @@ import {Component, OnInit, ChangeDetectionStrategy, Input} from '@angular/core';
 @Component({
   selector: 'app-notice',
   templateUrl: './notice.component.html',
-  styleUrls: ['./notice.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NoticeComponent implements OnInit {
   @Input() data!: { label: string };
-  constructor() { }
+  i: {
+    password: boolean;
+    messages: boolean;
+    todo: boolean;
+  } = {
+    password: true,
+    messages: true,
+    todo: true,
+  };
+
+  constructor() {
+  }
 
   ngOnInit(): void {
     console.log(this.data);
