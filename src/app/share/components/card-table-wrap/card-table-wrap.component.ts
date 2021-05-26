48117161a68430850cdbbb8c03a287d74ad9a8cc
huajian <287643967@qq.com>
2021-05-26 14:21:45 +0800

    提交代码

--- src/app/share/components/card-table-wrap/card-table-wrap.component.ts
+++ src/app/share/components/card-table-wrap/card-table-wrap.component.ts
@@ -2,29 +2,31 @@ import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output}
 import {NzSafeAny} from 'ng-zorro-antd/core/types';
 
 interface ActionCodeObj {
-    add?: string;
+  add?: string;
 }
 
 @Component({
-    selector: 'app-card-table-wrap',
-    templateUrl: './card-table-wrap.component.html',
-    styleUrls: ['./card-table-wrap.component.less'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'app-card-table-wrap',
+  templateUrl: './card-table-wrap.component.html',
+  styleUrls: ['./card-table-wrap.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CardTableWrapComponent implements OnInit {
-    @Input() needAddBtn: boolean;
-    @Output() add = new EventEmitter<NzSafeAny>();
-    @Input() actionCode!: ActionCodeObj;
+  @Input() needTableTitle: boolean;
+  @Input() needAddBtn: boolean;
+  @Output() add = new EventEmitter<NzSafeAny>();
+  @Input() actionCode!: ActionCodeObj;
 
-    constructor() {
-        this.needAddBtn = false;
-    }
+  constructor() {
+    this.needAddBtn = false;
+    this.needTableTitle = true;
+  }
 
-    addBtnClick(): void {
-        this.add.emit();
-    }
+  addBtnClick(): void {
+    this.add.emit();
+  }
 
-    ngOnInit(): void {
-    }
+  ngOnInit(): void {
+  }
 
 }
