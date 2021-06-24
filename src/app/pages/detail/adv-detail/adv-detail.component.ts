d86d20d4a7d00c898cf338bf0009618b65bd5dc9
huajian <287643967@qq.com>
2021-06-24 19:19:50 +0800

    高级详情页完善

--- src/app/pages/detail/adv-detail/adv-detail.component.ts
+++ src/app/pages/detail/adv-detail/adv-detail.component.ts
@@ -1,7 +1,17 @@
 import {Component, OnInit, ChangeDetectionStrategy, ViewChild, TemplateRef, AfterViewInit} from '@angular/core';
 import {PageHeaderType} from '../../../share/components/page-header/page-header.component';
-import {inNextTick} from 'ng-zorro-antd/core/util';
 import {BreakpointObserver} from '@angular/cdk/layout';
+import {MyTableConfig} from '../../../share/components/ant-table/ant-table.component';
+
+interface ReturnObj {
+  num: string;
+  name: string;
+  code: string;
+  unitPrice: number;
+  number: string;
+  price: string;
+}
+
 
 @Component({
   selector: 'app-adv-detail',
@@ -13,7 +23,9 @@ export class AdvDetailComponent implements OnInit, AfterViewInit {
   @ViewChild('headerExtra', {static: false}) headerExtra!: TemplateRef<any>;
   @ViewChild('headerContent', {static: false}) headerContent!: TemplateRef<any>;
   @ViewChild('headerFooter', {static: false}) headerFooter!: TemplateRef<any>;
+  @ViewChild('highLightTpl', {static: true}) highLightTpl!: TemplateRef<any>;
   stepDirection: 'horizontal' | 'vertical' = 'horizontal';
+  returnTableConfig!: MyTableConfig;
   pageHeaderInfo: Partial<PageHeaderType> = {
     title: '',
     breadcrumb: [],
@@ -22,8 +34,86 @@ export class AdvDetailComponent implements OnInit, AfterViewInit {
     footer: '',
   };
 
+  returnDataList: ReturnObj[] = [{
+    num: '1234561',
+    name: '矿泉水 550ml',
+    code: '演示作用域',
+    unitPrice: 1233333,
+    number: '1',
+    price: '2.00',
+  }, {
+    num: '1234561',
+    name: '矿泉水 550ml',
+    code: '演示作用域',
+    unitPrice: 1233333,
+    number: '1',
+    price: '2.00',
+  }, {
+    num: '1234561',
+    name: '矿泉水 550ml',
+    code: '演示作用域',
+    unitPrice: 1233333,
+    number: '1',
+    price: '2.00',
+  }, {
+    num: '1234561',
+    name: '矿泉水 550ml',
+    code: '演示作用域',
+    unitPrice: 1233333,
+    number: '1',
+    price: '2.00',
+  }, {
+    num: '1234561',
+    name: '矿泉水 550ml',
+    code: '演示作用域',
+    unitPrice: 1233333,
+    number: '1',
+    price: '2.00',
+  }];
+
   constructor(private breakpointObserver: BreakpointObserver) {
   }
+
+  private initReturnTable(): void {
+    this.returnTableConfig = {
+      showCheckbox: false,
+      headers: [
+        {
+          title: '操作类型',
+          field: 'num',
+          width: 50,
+        },
+        {
+          title: '操作人',
+          width: 60,
+          field: 'name',
+        },
+        {
+          title: '执行结果',
+          width: 50,
+          field: 'code',
+          tdTemplate: this.highLightTpl,
+        },
+        {
+          title: '操作时间',
+          width: 50,
+          field: 'unitPrice',
+          pipe: 'date:yyyy-MM-dd HH:mm',
+        },
+        {
+          title: '备注',
+          width: 50,
+          field: 'number',
+        },
+      ],
+      total: 0,
+      loading: false,
+      pageSize: 10,
+      pageIndex: 1,
+    };
+  }
+
+
 
   ngOnInit(): void {
     this.breakpointObserver.observe(['(max-width: 770px)']).subscribe(result => {
@@ -33,6 +123,7 @@ export class AdvDetailComponent implements OnInit, AfterViewInit {
         this.stepDirection = 'horizontal';
       }
     });
+    this.initReturnTable();
   }
 
   ngAfterViewInit(): void {
