e2f9b45c1bafc9c1382291e5ed048876ee500642
huajian <287643967@qq.com>
2021-07-28 14:43:52 +0800

    优化联系客服代码

--- src/app/share/components/chat/chat.component.ts
+++ src/app/share/components/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ChangeDetectionStrategy, ViewChild, ElementRef, AfterViewChecked, OnDestroy, Output, EventEmitter} from '@angular/core';
+import {Component, OnInit, ChangeDetectionStrategy, ViewChild, ElementRef, OnDestroy, Output, EventEmitter} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
 
 
@@ -8,7 +8,7 @@ import {FormBuilder, FormGroup} from '@angular/forms';
   styleUrls: ['./chat.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ChatComponent implements OnInit, AfterViewChecked, OnDestroy {
+export class ChatComponent implements OnInit, OnDestroy {
   @ViewChild('scrollMe') private myScrollContainer!: ElementRef;
   @Output() changeShows = new EventEmitter<boolean>();
   validateForm!: FormGroup;
@@ -22,25 +22,22 @@ export class ChatComponent implements OnInit, AfterViewChecked, OnDestroy {
     console.log('客服功能销毁了');
   }
 
-  ngAfterViewChecked(): void {
-    this.scrollToBottom();
-  }
-
   close(): void {
     this.changeShows.emit(false);
   }
 
   scrollToBottom(): void {
     try {
-      console.log('这里会一直调用，如果不需要客服功能，直接把这个chat组件删除掉，避免性能浪费');
       this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
     } catch (err) {
     }
   }
 
-  submitDesc(value: string): void {
-    this.userSubmitArray.push(value);
-    this.validateForm.get('question')?.reset();
+  sendMessage(msg: string): void {
+    this.userSubmitArray.push(msg);
+    setTimeout(() => {
+      this.validateForm.get('question')?.setValue(null);
+    });
   }
 
   ngOnInit(): void {
