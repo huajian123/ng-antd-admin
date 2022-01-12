75752c310796cc64fd889d452dd7b8d8b23e5f99
huajian <287643967@qq.com>
2022-01-12 17:01:23 +0800

    修复全屏时esc按钮不能被监听的bug

--- src/app/shared/directives/toggle-fullscreen.directive.ts
+++ src/app/shared/directives/toggle-fullscreen.directive.ts
@@ -1,19 +1,24 @@
-import {Directive, HostListener} from '@angular/core';
+import {Directive, HostListener, OnInit} from '@angular/core';
 import screenfull from "screenfull";
 
 @Directive({
   selector: '[appToggleFullscreen]',
   exportAs: 'appToggleFullscreen'
 })
-export class ToggleFullscreenDirective {
-
+export class ToggleFullscreenDirective implements OnInit {
   isFullscreenFlag = true;
+
+  ngOnInit(): void {
+    screenfull.onchange(() => {
+      setTimeout(() => {
+        this.isFullscreenFlag = !this.isFullscreenFlag;
+      },10)
+    })
+  }
 
   @HostListener("click") onClick() {
     if (screenfull.isEnabled) {
-      screenfull.toggle().then(() => {
-        this.isFullscreenFlag = !this.isFullscreenFlag;
-      })
+      screenfull.toggle();
     }
   }
 }
