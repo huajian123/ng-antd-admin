cac92cd22303c8627c793951dc5cd218a0b19630
huajian <287643967@qq.com>
2022-04-24 08:24:07 +0800

    msg优化

--- src/app/pages/feat/feat-routing.module.ts
+++ src/app/pages/feat/feat-routing.module.ts
@@ -4,6 +4,7 @@ import {RouterModule, Routes} from '@angular/router';
 const routes: Routes = [
   {path: 'ex-modal', loadChildren: () => import('./ex-modal/ex-modal.module').then(m => m.ExModalModule)},
   {path: 'ex-drawer', loadChildren: () => import('./ex-drawer/ex-drawer.module').then(m => m.ExDrawerModule)},
+  {path: 'msg', loadChildren: () => import('./msg/msg.module').then(m => m.MsgModule)},
   {path: 'rich-text', loadChildren: () => import('./rich-text/rich-text.module').then(m => m.RichTextModule)},
   {path: 'context-menu', loadChildren: () => import('./context-menu/context-menu.module').then(m => m.ContextMenuModule)},
   {path: 'click-out-side', loadChildren: () => import('./click-out-side/click-out-side.module').then(m => m.ClickOutSideModule)},
