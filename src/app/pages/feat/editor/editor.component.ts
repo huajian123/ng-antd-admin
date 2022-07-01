9e17b6cc147095ea724995a662d5a8bf75d53764
huajian <287643967@qq.com>
2022-07-01 08:05:32 +0800

    fix(module:editor): fixed editor code

--- src/app/pages/feat/editor/editor.component.ts
+++ src/app/pages/feat/editor/editor.component.ts
@@ -1,23 +1,24 @@
-import { Component, OnInit, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
-import { PageHeaderType } from "@shared/components/page-header/page-header.component";
+import {Component, OnInit, OnDestroy, ChangeDetectionStrategy} from '@angular/core';
+import {PageHeaderType} from "@shared/components/page-header/page-header.component";
 
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
-  styleUrls: ['./editor.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class EditorComponent implements OnInit {
+export class EditorComponent implements OnInit, OnDestroy {
   pageHeaderInfo: Partial<PageHeaderType> = {
     title: '代码编辑器，vscode的起源',
     breadcrumb: ['首页', '扩展功能', '代码编辑器'],
   };
   editorOptions = {theme: 'vs-dark', language: 'javascript'};
-  code: string= 'function x() {\n\tconsole.log("Hello world!");\n}';
-  constructor() { }
+  code: string = 'function x() {\n\tconsole.log("Hello world!");\n}';
+
+  constructor() {
+  }
 
   initEditor(): void {
-    
+
   }
 
   ngOnInit(): void {
--- src/app/pages/feat/editor/editor.module.ts
+++ src/app/pages/feat/editor/editor.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
 import { EditorRoutingModule } from './editor-routing.module';
 import { EditorComponent } from './editor.component';
 import {SharedModule} from "@shared/shared.module";
-
 import { MonacoEditorModule } from 'ngx-monaco-editor';
 
 
@@ -14,7 +11,6 @@ import { MonacoEditorModule } from 'ngx-monaco-editor';
   ],
   imports: [
     SharedModule,
-    // EditorModule,
     EditorRoutingModule,
     MonacoEditorModule.forRoot()
   ]
