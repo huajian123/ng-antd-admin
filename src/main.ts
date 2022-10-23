c5ec15c0cad51922d9b78c614f32e2836c96aa07
huajian123 <287643967@qq.com>
2022-10-23 21:36:27 +0800

    feat: 项目变更检测优化

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,11 @@ import { environment } from '@env/environment';
 if (environment.production) {
   enableProdMode();
 }
-
-platformBrowserDynamic().bootstrapModule(AppModule)
+/*
+ * https://netbasal.com/reduce-change-detection-cycles-with-event-coalescing-in-angular-c4037199859f
+ * ngZoneEventCoalescing: true
+ * 第一个表示事件冒泡的变更检测合并，第二个表示事件循环的变更检测合并
+ * */
+platformBrowserDynamic()
+  .bootstrapModule(AppModule, { ngZoneEventCoalescing: true, ngZoneRunCoalescing: true })
   .catch(err => console.error(err));
