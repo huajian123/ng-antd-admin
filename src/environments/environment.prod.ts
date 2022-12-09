a14af81861b43b5b2806b2ed2a2d88db64c91ff7
Nam <namandroid.it@gmail.com>
2022-12-09 20:16:35 +0700

    o nha

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
-export const ip = 'localhost';
+export const ip = '45.76.180.157';
 export const port = '3000';
-export const localUrl = `http://${ip}:${port}/api`;
+export const localUrl = `http://${ip}:${port}/api/`;
 
 export const environment = {
   production: true
