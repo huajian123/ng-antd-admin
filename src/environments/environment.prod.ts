d07643f5e7c1d23f583db27602ae5a26972f0f8e
Nam <namandroid.it@gmail.com>
2023-01-10 19:28:56 +0700

    hihi

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 export const ip = '117.2.198.251';//45.76.180.157
-export const port = '3000';
+export const port = '3001';
 export const localUrl = `http://${ip}:${port}/api/`;
 
 export const environment = {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,12 @@
-/* eslint-disable prettier/prettier */
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-const ip = '1.117.181.242';
-const port = '80';
+const ip = '117.2.198.251';
+const port = '3001';
 export const localUrl = `http://${ip}:${port}`;
 
 export const environment = {
-  production: false
+  production: true
 };
 
 /*
