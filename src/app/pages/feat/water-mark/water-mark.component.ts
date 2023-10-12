5758be9e309f61fc91d9aee05a942f74237cce2d
huajian123 <287643967@qq.com>
2023-10-12 21:20:42 +0800

    chore: add water-mark,qrcode,task component

--- src/app/pages/feat/water-mark/water-mark.component.ts
+++ src/app/pages/feat/water-mark/water-mark.component.ts
@@ -1,14 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+import { PageHeaderComponent, PageHeaderType } from '@shared/components/page-header/page-header.component';
+import { NzWaterMarkModule } from 'ng-zorro-antd/water-mark';
 
 @Component({
   selector: 'app-water-mark',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NzWaterMarkModule, PageHeaderComponent],
   templateUrl: './water-mark.component.html',
   styleUrls: ['./water-mark.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class WaterMarkComponent {
-
+  pageHeaderInfo: Partial<PageHeaderType> = {
+    title: '水印',
+    breadcrumb: ['首页', '功能', '水印'],
+    desc: '没什么，搬运的zorro官网示例,https://ng.ant.design/components/water-mark/zh'
+  };
 }
