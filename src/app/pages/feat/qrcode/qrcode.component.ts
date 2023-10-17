cddd39052ffb47cec1a58236b5d7cd8cf0a7833e
huajian123 <287643967@qq.com>
2023-10-17 21:21:36 +0800

    feat:(module:qrcode): add base demo

--- src/app/pages/feat/qrcode/qrcode.component.ts
+++ src/app/pages/feat/qrcode/qrcode.component.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { PageHeaderComponent } from '@shared/components/page-header/page-header.component';
+import { NzQRCodeModule } from 'ng-zorro-antd/qr-code';
+
 @Component({
   selector: 'app-qrcode',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NzQRCodeModule, PageHeaderComponent],
   templateUrl: './qrcode.component.html',
   styleUrls: ['./qrcode.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush
