0d51b0ed708d319d73bcba8c410b1b7e0b46e54c
huajian123 <287643967@qq.com>
2023-11-02 22:12:11 +0800

    feat:(module:task): 完成左侧过滤条件的样式添加完整数据

--- src/app/pages/page-demo/task/task-search-info/task-search-filters/task-search-filters.component.ts
+++ src/app/pages/page-demo/task/task-search-info/task-search-filters/task-search-filters.component.ts
@@ -2,22 +2,53 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzListModule } from 'ng-zorro-antd/list';
 import { NzTypographyModule } from 'ng-zorro-antd/typography';
 
 @Component({
   selector: 'app-task-search-filters',
   standalone: true,
-  imports: [CommonModule, NzListModule, NzTypographyModule, NzAvatarModule],
+  imports: [CommonModule, NzListModule, NzTypographyModule, NzAvatarModule, NzIconModule],
   templateUrl: './task-search-filters.component.html',
   styleUrls: ['./task-search-filters.component.less']
 })
 export class TaskSearchFiltersComponent {
-  data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.'
+  data1 = [
+    {
+      color: 'red',
+      title: 'Meetings'
+    },
+    {
+      color: 'green',
+      title: 'Devops'
+    },
+    {
+      color: 'yellow',
+      title: 'Frontend'
+    },
+    {
+      color: 'blue',
+      title: 'Architecture'
+    },
+    {
+      color: 'pink',
+      title: 'API'
+    },
+    {
+      color: 'orange',
+      title: 'Issues'
+    },
+    {
+      color: 'purple',
+      title: 'Features'
+    },
+    {
+      color: 'red',
+      title: 'Other'
+    }
   ];
+
+  data2 = ['John Belinda', 'Reta Collen', 'Elizabeth Mozelle', 'Marys Rob', 'Adoree Morgan'];
+  data3 = ['2020-Q2', '2020-Q1', '2019-Q4', '2019-Q3'];
 }
--- src/app/pages/page-demo/task/task-search-info/task-search-info.component.ts
+++ src/app/pages/page-demo/task/task-search-info/task-search-info.component.ts
@@ -3,12 +3,13 @@ import { Component } from '@angular/core';
 
 import { TaskSearchFiltersComponent } from '@app/pages/page-demo/task/task-search-info/task-search-filters/task-search-filters.component';
 import { UserInfoComponent } from '@app/pages/page-demo/task/task-search-info/user-info/user-info.component';
+import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
 
 @Component({
   selector: 'app-task-search-info',
   standalone: true,
-  imports: [CommonModule, UserInfoComponent, TaskSearchFiltersComponent, NzDividerModule],
+  imports: [CommonModule, UserInfoComponent, TaskSearchFiltersComponent, NzDividerModule, NzCardModule],
   templateUrl: './task-search-info.component.html',
   styleUrls: ['./task-search-info.component.less']
 })
