f2c5989e80944bfdc2d8bd72a295f2c4ace260aa
huajian123 <287643967@qq.com>
2023-12-20 22:00:46 +0800

    feat: add mock

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from '@app/app.component';
 import { appConfig } from '@app/app.config';
 
-bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
+// bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
+
+async function prepareApp(): Promise<ServiceWorkerRegistration | undefined> {
+  const { worker } = await import('./mocks/browser');
+  return worker.start();
+}
+
+prepareApp().then(() => {
+  bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
+});
