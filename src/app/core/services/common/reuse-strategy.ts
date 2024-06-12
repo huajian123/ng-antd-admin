0908ab46b95923e043084e985f41e2b65e80d00f
huajian123 <287643967@qq.com>
2024-06-12 22:50:39 +0800

    style: 路由复用添加一段注释代码

--- src/app/core/services/common/reuse-strategy.ts
+++ src/app/core/services/common/reuse-strategy.ts
@@ -115,6 +115,7 @@ export class SimpleReuseStrategy implements RouteReuseStrategy {
   }
 
   // 获取存储路由
+  // 如果在这里获取目标路由组件的实例，执行生命周期，会导致组件的生命周期执行多次（大于等于2次），但是这样又能避免shouldReuseRoute里面用while
   retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle {
     const key = fnGetReuseStrategyKeyFn(route);
     return !key ? null : SimpleReuseStrategy.handlers[key];
