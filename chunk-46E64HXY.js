687077fe3f5f2fe064ef847680015b18274de602
huajian123 <287643967@qq.com>
2024-07-08 15:46:28 +0800

    Auto-generated commit

---
+++ chunk-46E64HXY.js
@@ -0,0 +1,1 @@
+import"./chunk-VB56BUGO.js";var o=[{path:"",redirectTo:"login1",pathMatch:"full"},{path:"login1",title:"\u7B2C\u4E00\u79CD\u767B\u5F55",data:{key:"login1"},loadComponent:()=>import("./chunk-R5MDVGFR.js").then(t=>t.Login1Component)}];export{o as default};
--- chunk-LN5VUSWD.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-VB56BUGO.js";var o=[{path:"",redirectTo:"login1",pathMatch:"full"},{path:"login1",title:"\u7B2C\u4E00\u79CD\u767B\u5F55",data:{key:"login1"},loadComponent:()=>import("./chunk-OTJSXHSV.js").then(t=>t.Login1Component)}];export{o as default};
