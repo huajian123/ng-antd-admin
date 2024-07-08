738e9b9598dc8454b8384e3c7940f4d69c225dd5
huajian123 <287643967@qq.com>
2024-07-08 14:00:17 +0800

    Auto-generated commit

--- chunk-4UIADGBV.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-VB56BUGO.js";var a=[{path:"",redirectTo:"flow-chat",pathMatch:"full"},{path:"flow-chat",title:"\u6D41\u7A0B\u56FE",data:{key:"flow-chat"},loadComponent:()=>import("./chunk-CARO5FKI.js").then(t=>t.FlowChatComponent)}];export{a as default};
