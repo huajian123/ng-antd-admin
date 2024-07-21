d0676472de65e1a589dfa4413b860b1efb3068a1
huajian123 <287643967@qq.com>
2024-07-21 16:29:17 +0800

    fix: init nest

---
+++ nest-api/src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets('public'); // 配置静态资源
+  await app.listen(3000);
+}
+bootstrap();
---
+++ ui/src/app/core/services/http/login/login.service.ts
@@ -0,0 +1,34 @@
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+// import { MENU_TOKEN } from '@config/menu';
+import { Menu } from '../../types';
+import { BaseHttpService } from '../base-http.service';
+// import { MenusService } from '@services/system/menus.service';
+
+export interface UserLogin {
+  name: string;
+  password: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  http = inject(BaseHttpService);
+  // private menus = inject(MENU_TOKEN);
+
+  public login(params: UserLogin): Observable<string> {
+    return this.http.post('/login', params, { needSuccessInfo: false });
+  }
+
+  public test(): Observable<string> {
+    return this.http.get('/getHello');
+  }
+
+  public getMenuByUserId(userId: number): Observable<Menu[]> {
+    // 如果是静态菜单，就把下面注释放开
+    // return of(this.menus);
+    return this.http.get(`/sysPermission/menu/${userId}`);
+  }
+}
