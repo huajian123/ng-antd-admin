ce517ee9b18795f3f026f4edb9784948429737a1
hujian123 <287643967@qq.com>
2026-04-21 09:01:08 +0800

    feat(module:ripple): 增加水波纹

--- ui/src/app/pages/feat/ripple/ripple.component.ts
+++ ui/src/app/pages/feat/ripple/ripple.component.ts
@@ -1,28 +1,26 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-// import { MatRippleModule } from '@angular/material/core';
 
 import { PageHeaderType, PageHeaderComponent } from '@shared/components/page-header/page-header.component';
+import { RippleDirective } from '@shared/directives/ripple.directive';
 
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
-import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
 
 @Component({
   selector: 'app-ripple',
   templateUrl: './ripple.component.html',
   styleUrl: './ripple.component.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [PageHeaderComponent, NzCardModule, NzCheckboxModule, FormsModule, NzInputNumberModule] // , ColorPickerDirective, MatRippleModule
+  imports: [PageHeaderComponent, NzCardModule, NzCheckboxModule, FormsModule, RippleDirective]
 })
 export class RippleComponent {
   pageHeaderInfo: Partial<PageHeaderType> = {
     title: '水波纹',
-    breadcrumb: ['首页', '功能', '水波纹']
+    breadcrumb: ['首页', '功能', '水波纹'],
+    desc: '另一个做法是引入material里面的MatRipple，我嫌弃不应该引入第二个组件库，这里手写了。本项目历史版本在v20以及以下版本有演示'
   };
-  centered = false;
-  disabled = false;
-  unbounded = false;
-  radius = 0;
-  color = 'red';
+  disabled = signal(false);
+  unbounded = signal(false);
+  color = signal('rgba(0,0,0,0.15)');
 }
