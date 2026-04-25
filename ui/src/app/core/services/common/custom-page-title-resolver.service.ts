52b291c7f979f3d2eb7ea6ef8aa0f2ef2548c5a8
hujian123 <287643967@qq.com>
2026-04-25 10:46:51 +0800

    feat: 增加tab右键多语言，以及切换语系时，浏览器title的多语言

--- ui/src/app/core/services/common/custom-page-title-resolver.service.ts
+++ ui/src/app/core/services/common/custom-page-title-resolver.service.ts
@@ -1,4 +1,5 @@
-import { inject, Injectable } from '@angular/core';
+import { DestroyRef, inject, Injectable } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { Title } from '@angular/platform-browser';
 import { RouterStateSnapshot, TitleStrategy } from '@angular/router';
 
@@ -10,12 +11,20 @@ import { TranslateService } from '@ngx-translate/core';
 export class CustomPageTitleResolverService extends TitleStrategy {
   readonly title = inject(Title);
   private translate = inject(TranslateService);
+  private destroyRef = inject(DestroyRef);
+  private lastRouterState: RouterStateSnapshot | null = null;
 
   constructor() {
     super();
+    this.translate.onLangChange.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(() => {
+      if (this.lastRouterState) {
+        this.updateTitle(this.lastRouterState);
+      }
+    });
   }
 
   override updateTitle(routerState: RouterStateSnapshot): void {
+    this.lastRouterState = routerState;
     const titleKey = this.buildTitle(routerState);
     if (titleKey !== undefined) {
       const title = this.translate.instant(titleKey) || titleKey;
