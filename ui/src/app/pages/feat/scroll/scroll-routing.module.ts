0bfa214ed7031b68c949881f6a20627ae3b53d59
hujian123 <287643967@qq.com>
2026-04-25 22:02:05 +0800

    fix: 修复tab多语言遗漏

--- ui/src/app/pages/feat/scroll/scroll-routing.module.ts
+++ ui/src/app/pages/feat/scroll/scroll-routing.module.ts
@@ -3,10 +3,10 @@ import { Route } from '@angular/router';
 export default [
   {
     path: 'keep-scroll-page',
-    title: '缓存滚动条',
+    title: 'menu.default:feat:scroll:keep-scroll-page',
     data: { key: 'keep-scroll-page', scrollContain: ['#div-scroll1', '#div-scroll2'] },
     loadComponent: () => import('./keep-scroll-page/keep-scroll-page.component').then(m => m.KeepScrollPageComponent)
   },
-  { path: 'play-scroll', title: '玩弄滚动条', data: { key: 'play-scroll' }, loadComponent: () => import('./play-scroll/play-scroll.component').then(m => m.PlayScrollComponent) },
+  { path: 'play-scroll', title: 'menu.default:feat:scroll:play-scroll', data: { key: 'play-scroll' }, loadComponent: () => import('./play-scroll/play-scroll.component').then(m => m.PlayScrollComponent) },
   { path: '', redirectTo: 'keep-scroll-page', pathMatch: 'full' }
 ] satisfies Route[];
