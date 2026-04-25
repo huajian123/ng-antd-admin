9da2827a58788675cab9459e052250131048f6bc
hujian123 <287643967@qq.com>
2026-04-25 08:54:35 +0800

    fix: 多语言tab文件漏写

--- ui/src/app/pages/page-demo/list/list-routing.module.ts
+++ ui/src/app/pages/page-demo/list/list-routing.module.ts
@@ -3,8 +3,13 @@ import { Route } from '@angular/router';
 export default [
   { path: '', redirectTo: 'search-table', pathMatch: 'full' },
   { path: 'search-table', loadChildren: () => import('./search-table/search-table-routing') },
-  { path: 'standard-table', title: '标准表格', data: { key: 'standard-table' }, loadComponent: () => import('./standard-table/standard-table.component').then(m => m.StandardTableComponent) },
-  { path: 'tree-list', title: '树状表格', data: { key: 'tree-list' }, loadComponent: () => import('./tree-list/tree-list.component').then(m => m.TreeListComponent) },
-  { path: 'card-table', title: '卡片列表', data: { key: 'card-table' }, loadComponent: () => import('./card-table/card-table.component').then(m => m.CardTableComponent) },
+  {
+    path: 'standard-table',
+    title: 'menu.default:page-demo:list:standard-table',
+    data: { key: 'standard-table' },
+    loadComponent: () => import('./standard-table/standard-table.component').then(m => m.StandardTableComponent)
+  },
+  { path: 'tree-list', title: 'menu.default:page-demo:list:tree-list', data: { key: 'tree-list' }, loadComponent: () => import('./tree-list/tree-list.component').then(m => m.TreeListComponent) },
+  { path: 'card-table', title: 'menu.default:page-demo:list:card-table', data: { key: 'card-table' }, loadComponent: () => import('./card-table/card-table.component').then(m => m.CardTableComponent) },
   { path: 'search-list', loadChildren: () => import('./search-list/search-list-routing') }
 ] satisfies Route[];
